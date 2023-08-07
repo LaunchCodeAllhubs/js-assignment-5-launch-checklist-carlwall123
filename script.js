b40fef0f3aeba0b3f9735e716b8aef4e24c7b3ce
CARLWALL123 <carlwall123@gmail.com>
2023-08-07 15:02:11 -0500

    submitting my assignment

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// Write your JavaScript code here!
+//const { myFetch } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
+       window.addEventListener('load',function(){
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+        let listedPlanets;
+        let selectedPlanet;
+        // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+        let listedPlanetsResponse = myFetch();
+        listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        }).then(function () {
+        selectedPlanet = pickPlanet(listedPlanets)
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star,selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image)
+        })
+
+        let list = document.getElementById('faultyItems')
+        let form = document.querySelector('form'); 
+        form.addEventListener('submit', function(event){
+        event.preventDefault();
+        let pilotName = document.querySelector('input[name=pilotName]' );
+        let coPilotName = document.querySelector('input[name=copilotName]');
+        let fuelLevel = document.querySelector('input[name=fuelLevel]');
+        let cargoMass = document.querySelector('input[name=cargoMass]');
+
+        formSubmission(document, list, pilotName, coPilotName, fuelLevel, cargoMass);
+        })
+    
 });
+
+
+
+
